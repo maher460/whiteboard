a374d012883e6ff53f458f9b8f2ab54ce8358713
Maryam Ahmed <maryam.a.alfehani@gmail.com>
2014-11-18 22:24:56 +0300

    Design, needs better quality sketches

--- whiteboard/sketch/views.py
+++ whiteboard/sketch/views.py
@@ -12,16 +12,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import permission_required
 import datetime
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def index(request):
+    c = {}
+    c.update(csrf(request))
     return render_to_response('sketch/index.html', {})
     
 def login(request):
     c = {}
     c.update(csrf(request))    
     return render_to_response('sketch/login.html', c)
-    
+
+@csrf_exempt    
 def auth_view(request):
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
@@ -36,6 +41,7 @@ def logout_view(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
     
+@csrf_exempt
 def register_user(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
